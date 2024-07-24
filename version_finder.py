e346c56797594bfd902bb2706434f63b7fc80d4a
kunalsuri <kunalsuri@outlook.com>
2024-07-24 10:44:53 +0200

    Create version_finder.py

---
+++ version_finder.py
@@ -0,0 +1,14 @@
+import numpy
+import torch
+
+# Numpy
+# To find the version of the Numpy installed in your system
+
+print("Numpy Version is : " + numpy.__version__)
+
+# PyTorch
+# To find the version of the PyTorch installed in your system
+# Latest version of the PyTorch can be downloaded via : https://pytorch.org/
+
+print("Torch Version is : " + torch.__version__)
+
